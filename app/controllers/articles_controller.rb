9b0141e95ca14331525d39888e83bbe2d651676b
Toru Suzuki <innovationlife.net@gmail.com>
2016-05-31 06:43:15 +0900

    Articleのフォームを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,27 @@ class ArticlesController < ApplicationController
 
   def show
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+
+    if @article.save
+      redirect_to @article,
+        notice: t("article_was_successfully_created")
+    else
+      render :new
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(
+      :title,
+      :body
+    )
+  end
 end
