eec7a2000595bad1b0172cfda8184bfcc05e9b96
Toru Suzuki <innovationlife.net@gmail.com>
2016-05-31 06:41:11 +0900

    タイポを修正

--- db/migrate/20160529232638_create_articles.rb
+++ db/migrate/20160529232638_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
       t.text :body
-      t.integer :uesr_id
+      t.integer :user_id
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20160529234252) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.integer  "uesr_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160529234252) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
