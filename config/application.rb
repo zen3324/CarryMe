e561c68baa39d1e31cfbc3d7e59523eb80503e2f
Toru Suzuki <innovationlife.net@gmail.com>
2016-05-30 09:06:26 +0900

    slimを導入

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module CarryMe
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators.template_engine = :slim
   end
 end
